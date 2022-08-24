9caf117e525369e6d26270df066476fe0e9999cd
Roqeeb <77676155+roqeebYusuff@users.noreply.github.com>
2022-08-24 06:24:28 +0100

    Added start script

--- server.js
+++ server.js
@@ -44,17 +44,18 @@ app.get("/api", (req, res) => {
   });
 });
 
+app.use(express.static(path.resolve(__dirname, "./client/build")));
 // This middleware informs the express application to serve our compiled React files
-if (
-  process.env.NODE_ENV === "production" ||
-  process.env.NODE_ENV === "staging"
-) {
-  app.use(express.static(path.resolve(__dirname, "./client/build")));
+// if (
+//   process.env.NODE_ENV === "production" ||
+//   process.env.NODE_ENV === "staging"
+// ) {
+//   app.use(express.static(path.resolve(__dirname, "./client/build")));
 
-  app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "./client/build", "index.html"));
-  });
-}
+//   app.get("*", function (req, res) {
+//     res.sendFile(path.join(__dirname, "./client/build", "index.html"));
+//   });
+// }
 
 app.listen(process.env.PORT || 7000, function () {
   console.log("Server running on port " + (process.env.PORT || 7000));
