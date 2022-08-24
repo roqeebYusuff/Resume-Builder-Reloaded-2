c4b622c56858a783c9d5b6ef5681e57dc7915234
Roqeeb <77676155+roqeebYusuff@users.noreply.github.com>
2022-08-24 05:33:12 +0100

    Added start script

--- server.js
+++ server.js
@@ -49,11 +49,11 @@ if (
   process.env.NODE_ENV === "production" ||
   process.env.NODE_ENV === "staging"
 ) {
-  app.use(express.static(path.join(__dirname, "client/build")));
+  app.use(express.static(path.resolve(__dirname, "./client/build")));
 
-  app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "client/build", "index.html"));
-  });
+  // app.get("*", function (req, res) {
+  //   res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  // });
 }
 
 app.listen(process.env.PORT || 7000, function () {
